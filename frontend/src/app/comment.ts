042c9f47b8d6a5a622e0d6b196ff89738486111d
janerque <64560065+janerque@users.noreply.github.com>
2021-04-23 00:57:14 +0600

    we going to add changing files

--- frontend/src/app/comment.ts
+++
@@ -1,9 +0,0 @@
-export class Comment {
-  email: string;
-  review: string;
-  value: number;
-  professor: number;
-
-  constructor() {
-  }
-}
--- frontend/src/app/scripts.ts
+++
@@ -1,8 +0,0 @@
-export const scripts = [
-  '/assets/js/swiper.min.js',
-  '/assets/js/swiper.js',
-  '/assets/js/jquery.js',
-  '/assets/js/masonry.pkgd.min.js',
-  '/assets/js/jquery.collapsible.min.js',
-  '/assets/js/custom.js',
-];
